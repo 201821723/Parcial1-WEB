9558c2fc0a9424e9695973a4cc7c803964c818f9
Sara Plazas Copete <s.plazas@uniandes.edu.co>
2022-03-16 13:40:01 -0500

    Funcionalidad 3 fea

--- Parcial.js
+++ Parcial.js
@@ -8,6 +8,8 @@ let array = [];
 
 let labelCarrito = document.getElementById('contador');
 let numCarrito = 0;
+let pedido = [];
+let carrito = document.getElementById('carrito');
 
 function getData(callback) {
     fetch(url).then(res => res.json()).then(res => {
@@ -19,6 +21,8 @@ function mostrarCate(nombreCat) {
     getData((value) => {
 
         array = value;
+
+        div.innerHTML = "";
 
         let h2 = document.createElement('h2');
         h2.style = "text-align: center; margin-bottom: 30px"
@@ -81,16 +85,134 @@ function mostrarCate(nombreCat) {
 
                      a.addEventListener("click", function () {
                         labelCarrito.innerHTML = (numCarrito + 1) + " items"
-                        numCarrito = numCarrito + 1;
+                         numCarrito = numCarrito + 1;
+
+                         let esta = "false";
+
+                         for (i in pedido) {
+                             if (pedido[i][1] == prods["name"]) {
+                                 pedido[i][0] = pedido[i][0] + 1
+                                 esta = "true";
+                             }
+                         }
+
+                         if (pedido.length == 0 || esta == "false") {
+                             let producto = [];
+                             producto.push(1);
+                             producto.push(prods["name"]);
+                             producto.push(prods["price"]);
+
+                             pedido.push(producto);
+                         }
                     });
                 }
-
                 div.appendChild(div0);
-
             }
         }
     });
 };
+
+carrito.addEventListener("click", function () {
+
+    div.innerHTML = "";
+
+    let h2 = document.createElement('h2');
+    h2.style = "text-align: center; margin-bottom: 30px"
+    h2.innerHTML = "ORDER DETAIL";
+    div.appendChild(h2);
+
+    let table = document.createElement('table');
+    table.className = "table table-striped";
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    let row_1 = document.createElement('tr');
+    let heading_1 = document.createElement('th');
+    heading_1.scope = "col"
+    heading_1.innerHTML = "Item"
+
+    let heading_2 = document.createElement('th');
+    heading_2.scope = "col"
+    heading_2.innerHTML = "Quantity"
+
+
+    let heading_3 = document.createElement('th');
+    heading_3.scope = "col"
+    heading_3.innerHTML = "Description"
+
+    let heading_4 = document.createElement('th');
+    heading_4.scope = "col"
+    heading_4.innerHTML = "Unit price"
+
+    let heading_5 = document.createElement('th');
+    heading_5.scope = "col"
+    heading_5.innerHTML = "Amount"
+
+    let heading_6 = document.createElement('th');
+    heading_6.scope = "col"
+    heading_6.innerHTML = "Modify"
+
+    row_1.appendChild(heading_1)
+    row_1.appendChild(heading_2)
+    row_1.appendChild(heading_3)
+    row_1.appendChild(heading_4)
+    row_1.appendChild(heading_5)
+    row_1.appendChild(heading_6)
+    thead.appendChild(row_1)
+
+    let precioTotal = 0;
+
+    for (i in pedido) {
+        let row = document.createElement('tr');
+        let th = document.createElement('th');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        let td4 = document.createElement('td');
+
+        th.scope = "row";
+
+        let valor = pedido[i]
+
+        th.innerHTML = i
+        td1.innerHTML = valor[0]
+        td2.innerHTML = valor[1]
+        td3.innerHTML = valor[2]
+        td4.innerHTML = valor[0] * valor[2]
+
+        precioTotal = precioTotal + parseFloat(td4.innerHTML)
+
+        row.appendChild(th);
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td4);
+
+        tbody.appendChild(row);
+    }
+
+    let h6 = document.createElement('h6');
+    h6.innerHTML = "Total: " + precioTotal;
+    table.appendChild(h6);
+
+    let a = document.createElement('a');
+    a.href = "#"
+    a.className = "btn btn-primary"
+    a.style = "margin-right: 10px; background-color: #f44336;"
+    a.innerHTML = "Cancel"
+    table.appendChild(a);
+
+    let a1 = document.createElement('a');
+    a1.href = "#"
+    a1.className = "btn btn-primary"
+    a1.innerHTML = "Confirm order"
+    table.appendChild(a1);
+
+    div.appendChild(table);
+});
 
 
 document.getElementById('Events').appendChild(div);
