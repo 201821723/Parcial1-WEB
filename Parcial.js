a9c9092a88feb36c3d2e0644ec0bce7ae4958014
Sara Plazas Copete <s.plazas@uniandes.edu.co>
2022-03-16 01:57:29 -0500

    Funcionalidad 2

--- Parcial.js
+++ Parcial.js
@@ -6,22 +6,31 @@ const url = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f
 //Datos
 let array = [];
 
+let labelCarrito = document.getElementById('contador');
+let numCarrito = 0;
+
 function getData(callback) {
     fetch(url).then(res => res.json()).then(res => {
         callback(res);
     })
 }
 
-
-function mostrarBurguers(){
+function mostrarCate(nombreCat) {
     getData((value) => {
 
         array = value;
 
+        let h2 = document.createElement('h2');
+        h2.style = "text-align: center; margin-bottom: 30px"
+        h2.innerHTML = nombreCat;
+        div.appendChild(h2);
+
         for (i in array) {
             let nameYprods = array[i]
 
-            if (nameYprods["name"] == "Burguers") {
+            console.log(nombreCat)
+
+            if (nameYprods["name"] === nombreCat) {
 
                 let div0 = document.createElement('div');
                 div0.className = "card-deck"
@@ -54,11 +63,12 @@ function mostrarBurguers(){
 
                     let a = document.createElement('a');
                     a.href = "#"
-                    a.className = "btn btn-primary"
+                    a.className ="btn btn-primary"
 
                     h5.innerHTML = prods["name"]
                     p.innerHTML = prods["description"]
                     small.innerHTML = prods["price"]
+                    a.innerHTML = "Add to cart"
 
                     div1.appendChild(img);
                     div1.appendChild(div2);
@@ -68,262 +78,11 @@ function mostrarBurguers(){
                     div2.appendChild(a);
 
                     div0.appendChild(div1);
-                }
 
-                div.appendChild(div0);
-
-            }
-        }
-    });
-};
-
-function mostrarTacos() {
-    getData((value) => {
-
-        array = value;
-
-        for (i in array) {
-            let nameYprods = array[i]
-
-            if (nameYprods["name"] == "Tacos") {
-
-                let div0 = document.createElement('div');
-                div0.className = "card-deck"
-
-                for (j in nameYprods["products"]) {
-                    let prods = nameYprods["products"][j]
-
-                    let div1 = document.createElement('div');
-                    div1.className = "card border-dark mb-3"
-                    div1.style = "width: 18rem"
-
-                    let img = document.createElement('img');
-                    img.className = "card-img-top"
-                    img.src = prods["image"]
-                    img.style = "width:90%; margin-top:10px"
-
-                    let div2 = document.createElement('div');
-                    div2.className = "card-body"
-
-                    let h5 = document.createElement('h5');
-                    h5.className = "card-title"
-                    h5.style = "text-align:center"
-
-                    let p = document.createElement('p');
-                    p.className = "card-text"
-                    p.style = "text-align:center"
-
-                    let small = document.createElement('small');
-                    small.className = "text-muted"
-
-                    let a = document.createElement('a');
-                    a.href = "#"
-                    a.className = "btn btn-primary"
-
-                    h5.innerHTML = prods["name"]
-                    p.innerHTML = prods["description"]
-                    small.innerHTML = prods["price"]
-
-                    div1.appendChild(img);
-                    div1.appendChild(div2);
-                    div2.appendChild(h5);
-                    div2.appendChild(p);
-                    p.appendChild(small);
-                    div2.appendChild(a);
-
-                    div0.appendChild(div1);
-                }
-
-                div.appendChild(div0);
-
-            }
-        }
-    });
-};
-
-function mostrarSalads() {
-    getData((value) => {
-
-        array = value;
-
-        for (i in array) {
-            let nameYprods = array[i]
-
-            if (nameYprods["name"] == "Salads") {
-
-                let div0 = document.createElement('div');
-                div0.className = "card-deck"
-
-                for (j in nameYprods["products"]) {
-                    let prods = nameYprods["products"][j]
-
-                    let div1 = document.createElement('div');
-                    div1.className = "card border-dark mb-3"
-                    div1.style = "width: 18rem"
-
-                    let img = document.createElement('img');
-                    img.className = "card-img-top"
-                    img.src = prods["image"]
-                    img.style = "width:90%; margin-top:10px"
-
-                    let div2 = document.createElement('div');
-                    div2.className = "card-body"
-
-                    let h5 = document.createElement('h5');
-                    h5.className = "card-title"
-                    h5.style = "text-align:center"
-
-                    let p = document.createElement('p');
-                    p.className = "card-text"
-                    p.style = "text-align:center"
-
-                    let small = document.createElement('small');
-                    small.className = "text-muted"
-
-                    let a = document.createElement('a');
-                    a.href = "#"
-                    a.className = "btn btn-primary"
-
-                    h5.innerHTML = prods["name"]
-                    p.innerHTML = prods["description"]
-                    small.innerHTML = prods["price"]
-
-                    div1.appendChild(img);
-                    div1.appendChild(div2);
-                    div2.appendChild(h5);
-                    div2.appendChild(p);
-                    p.appendChild(small);
-                    div2.appendChild(a);
-
-                    div0.appendChild(div1);
-                }
-
-                div.appendChild(div0);
-
-            }
-        }
-    });
-};
-
-function mostrarDesserts() {
-    getData((value) => {
-
-        array = value;
-
-        for (i in array) {
-            let nameYprods = array[i]
-
-            if (nameYprods["name"] == "Desserts") {
-
-                let div0 = document.createElement('div');
-                div0.className = "card-deck"
-
-                for (j in nameYprods["products"]) {
-                    let prods = nameYprods["products"][j]
-
-                    let div1 = document.createElement('div');
-                    div1.className = "card border-dark mb-3"
-                    div1.style = "width: 18rem"
-
-                    let img = document.createElement('img');
-                    img.className = "card-img-top"
-                    img.src = prods["image"]
-                    img.style = "width:90%; margin-top:10px"
-
-                    let div2 = document.createElement('div');
-                    div2.className = "card-body"
-
-                    let h5 = document.createElement('h5');
-                    h5.className = "card-title"
-                    h5.style = "text-align:center"
-
-                    let p = document.createElement('p');
-                    p.className = "card-text"
-                    p.style = "text-align:center"
-
-                    let small = document.createElement('small');
-                    small.className = "text-muted"
-
-                    let a = document.createElement('a');
-                    a.href = "#"
-                    a.className = "btn btn-primary"
-
-                    h5.innerHTML = prods["name"]
-                    p.innerHTML = prods["description"]
-                    small.innerHTML = prods["price"]
-
-                    div1.appendChild(img);
-                    div1.appendChild(div2);
-                    div2.appendChild(h5);
-                    div2.appendChild(p);
-                    p.appendChild(small);
-                    div2.appendChild(a);
-
-                    div0.appendChild(div1);
-                }
-
-                div.appendChild(div0);
-
-            }
-        }
-    });
-};
-
-function mostrarDS() {
-    getData((value) => {
-
-        array = value;
-
-        for (i in array) {
-            let nameYprods = array[i]
-
-            if (nameYprods["name"] == "Drinks and Sides") {
-
-                let div0 = document.createElement('div');
-                div0.className = "card-deck"
-
-                for (j in nameYprods["products"]) {
-                    let prods = nameYprods["products"][j]
-
-                    let div1 = document.createElement('div');
-                    div1.className = "card border-dark mb-3"
-                    div1.style = "width: 18rem"
-
-                    let img = document.createElement('img');
-                    img.className = "card-img-top"
-                    img.src = prods["image"]
-                    img.style = "width:90%; margin-top:10px"
-
-                    let div2 = document.createElement('div');
-                    div2.className = "card-body"
-
-                    let h5 = document.createElement('h5');
-                    h5.className = "card-title"
-                    h5.style = "text-align:center"
-
-                    let p = document.createElement('p');
-                    p.className = "card-text"
-                    p.style = "text-align:center"
-
-                    let small = document.createElement('small');
-                    small.className = "text-muted"
-
-                    let a = document.createElement('a');
-                    a.href = "#"
-                    a.className = "btn btn-primary"
-
-                    h5.innerHTML = prods["name"]
-                    p.innerHTML = prods["description"]
-                    small.innerHTML = prods["price"]
-
-                    div1.appendChild(img);
-                    div1.appendChild(div2);
-                    div2.appendChild(h5);
-                    div2.appendChild(p);
-                    p.appendChild(small);
-                    div2.appendChild(a);
-
-                    div0.appendChild(div1);
+                     a.addEventListener("click", function () {
+                        labelCarrito.innerHTML = (numCarrito + 1) + " items"
+                        numCarrito = numCarrito + 1;
+                    });
                 }
 
                 div.appendChild(div0);
